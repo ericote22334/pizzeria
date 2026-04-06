5edf8b8de7e971ce1ae2e3caa03a40dbff1033ef
SanNico22 <santiagonicora07@gmail.com>
2026-04-06 18:42:49 -0300

    reestructuracion de la conexion

--- Backend/conexion.php
+++ Backend/conexion.php
@@ -1,12 +1,7 @@
 <?php
-$user="root";
-$pass="";
-$server="localhost";
-$db="pizzeria";
-$conexion=mysqli_connect($server,$user,$pass) or die("error al conectar a la base de datos".mysql_error());
-mysqli_select_db($conexion,$db);
+$conexion = mysqli_connect("localhost", "root", "", "pizzeria");
 
-if($conexion->connect_error) {
-    die("Connection failed: ".$conexion->connect_error);
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
 ?>
