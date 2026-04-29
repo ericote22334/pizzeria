647b0bf7714aee9e19b2b1b071fedbac2d662f17
SanNico22 <santiagonicora07@gmail.com>
2026-04-29 19:36:38 -0300

    agregar ingredientes

---
+++ Backend/agregar_producto.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include("conexion.php");
+
+if (!isset($_SESSION['id_sucursal'])) {
+    header("Location: seleccionar_sucursal.php");
+    exit();
+}
+
+$id_sucursal = $_SESSION['id_sucursal'];
+
+// AGREGAR PRODUCTO
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['nombre'];
+    $stock_actual = $_POST['stock'];
+    $unidad = $_POST['unidad'];
+
+    $sql = "INSERT INTO ingredientes (nombre, stock_actual, unidad, id_sucursal) 
+            VALUES ('$nombre', '$stock_actual', '$unidad', '$id_sucursal')";
+
+    mysqli_query($conexion, $sql);
+
+    header("Location: ../agregar-producto.html");
+    exit();
+}
+?>
